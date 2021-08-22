5dade8b849c817f1a4744c74a81a52df9a823268
Luker <luker.plus@gmail.com>
2021-08-22 12:22:00 +0200

    commit pls

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from PIL import Image
+import numpy as np
 import os
 
 def make_square(input_image, fill_color=(0, 0, 200), min_size=300):
@@ -11,6 +12,31 @@ def make_square(input_image, fill_color=(0, 0, 200), min_size=300):
 
     return new_input_image
 
+def change_color(im):
+    im = im.convert('RGBA')
+
+    data = np.array(im)  # "data" is a height x width x 4 numpy array
+    red, green, blue, alpha = data.T  # Temporarily unpack the bands for readability
+
+    # Replace white with red... (leaves alpha values alone...)
+    white_areas = (red == blue) & (blue == green) & (128 <= red) & (red <= 140)
+    data[..., :-1][white_areas.T] = (0, 0, 0)  # Transpose back needed
+
+    return Image.fromarray(data)
+
+def blackout_input(input, target):
+    im1 = target.convert('RGB')
+    im2 = input.convert('RGB')
+
+    data1 = np.array(im1)  # "data" is a height x width x 4 numpy array
+    data2 = np.array(im2)  # "data" is a height x width x 4 numpy array
+    red, green, blue = data1.T  # Temporarily unpack the bands for readability
+
+    # Replace white with red... (leaves alpha values alone...)
+    white_areas = (red == 0) & (blue == 0) & (green == 0)
+    data2[...][white_areas.T] = (0, 0, 0)  # Transpose back needed
+
+    return Image.fromarray(data2)
 
 # for file in os.listdir("./data/Roofs/input/"):
 #     im1 = Image.open("./data/Roofs/input/" + file)
@@ -21,17 +47,18 @@ def make_square(input_image, fill_color=(0, 0, 200), min_size=300):
 #     im1 = im1.convert('RGB')
 #     im1.save("./data/roof/real/" + file[:-3] + "jpg")
 
+target = []
 for file in os.listdir("./data/dataset1"):
     if file[-3:] == "jpg":
-        im1 = Image.open("./data/dataset1/" + file)
+        target = make_square(change_color(Image.open("./data/dataset1/" + file)),(0,0,0))
 
-        make_square(im1,(0,0,0)).save("./data/combined_dataset/real/" + file)
+        target.save("./data/combined_dataset/real/" + file)
 
     if file[-3:] == "png":
         im1 = Image.open("./data/dataset1/" + file)
         im1 = im1.convert('RGB')
 
-        make_square(im1,(0,0,0)).save("./data/combined_dataset/input/" + file[:-3] + "jpg")
+        blackout_input(make_square(im1,(0,0,0)), target).save("./data/combined_dataset/input/" + file[:-3] + "jpg")
 
 # for file in os.listdir("./data/dataset2/trainA"):
 #     im1 = Image.open("./data/dataset2/trainA/" + file)
